6c85ae500c1c6d4bed2731484e17cc50c588d42c
CaioVilarouca <caiovilarouca@gmail.com>
2025-02-02 20:30:01 -0300

    print CSV

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,22 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Analise de Movimentação de Produtos (A-MIP)");
+		File fileCVS = new File("/home/caio/eclipse-workspace/amip/amip/mip.csv");
+		
+		System.out.println("Analise de Movimentação de Produtos (A-MIP)\n");
+		try {
+			Scanner scanner = new Scanner(fileCVS);
+			String line;
+			while(scanner.hasNext()) {
+				line = scanner.nextLine();
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+		}
 	}
 }
