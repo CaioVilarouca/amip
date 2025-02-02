f90edfda98f1b342973ea63d683845323b3f448c
CaioVilarouca <caiovilarouca@gmail.com>
2025-02-02 20:41:10 -0300

    up code

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,16 +7,19 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		File fileCVS = new File("/home/caio/eclipse-workspace/amip/amip/mip.csv");
-		
+		String fileLine;
+
 		System.out.println("Analise de Movimentação de Produtos (A-MIP)\n");
+		
 		try {
 			Scanner scanner = new Scanner(fileCVS);
-			String line;
 			while(scanner.hasNext()) {
-				line = scanner.nextLine();
-				System.out.println(line);
+				fileLine = scanner.nextLine();
+				System.out.println(fileLine);
 			}
+			scanner.close();
 		} catch (FileNotFoundException e) {
 		}
+
 	}
 }
