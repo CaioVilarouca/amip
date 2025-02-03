2b718d53bf5ad40a427e1231055d48af91d93540
CaioVilarouca <caiovilarouca@gmail.com>
2025-02-02 22:32:02 -0300

    start manipulation of date

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,13 +5,11 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import entites.Product;
+import entites.VerificationData;
 
 public class Program {
 	public static void main(String[] args) {
 		File fileCVS = new File("/home/caio/eclipse-workspace/amip/amip/mip.csv");
-		String fileLine;
-		int consico;
-		double value;
 
 		System.out.println("Analise de Movimentação de Produtos (A-MIP)\n");
 		
@@ -20,14 +18,17 @@ public class Program {
 			scanner.nextLine();// BREAK na primeira linha
 			
 			while(scanner.hasNext()) {
-				fileLine = scanner.nextLine();
+				String fileLine = scanner.nextLine();
 				String[] separationComma = fileLine.split(",");
 				
-				consico = Integer.parseInt(separationComma[0]);
-				value = Double.parseDouble(separationComma[2]);
-				Product product = new Product(consico, separationComma[1],value);
-				System.out.println(product);
+				int consico = Integer.parseInt(separationComma[0]);// Consico
+				double amount = Double.parseDouble(separationComma[2]);// Quantidade a quebra
+				double value = Double.parseDouble(separationComma[3]);// Preço de custo
+				double stock = Double.parseDouble(separationComma[4]);// Estoque Atual
 
+				Product product = new Product(consico, separationComma[1],value);
+				VerificationData vericationData = new VerificationData(product, amount, stock);
+				System.out.println(vericationData);
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
