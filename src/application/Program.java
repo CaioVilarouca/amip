41ae3a2817c582d7bcbe3017ce582bc8d67b87ea
CaioVilarouca <caiovilarouca@gmail.com>
2025-02-04 14:16:15 -0300

    Upgrade

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import entites.ManipulateData;
+import entites.Product;
+
 public class Program {
 	public static void main(String[] args) {
 		String url = "/home/caio/eclipse-workspace/amip/amip/mip.csv";
@@ -14,22 +17,22 @@ public class Program {
 		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(url))){
 			String line = bufferedReader.readLine();// read line end CSV
 			
+			line = bufferedReader.readLine();
+
 			while(line != null) {
-				System.out.println(line);
-				line = bufferedReader.readLine();
-				
-				/*
-				 * 		String fileLine = scanner.nextLine();
-				String[] separationComma = fileLine.split(";");
+				String[] separationComma = line.split(";");
 				
 				int consico = Integer.parseInt(separationComma[0]);
 				double quantity = Double.parseDouble(separationComma[2]);
 				double value = Double.parseDouble(separationComma[3]);
 				double stock = Double.parseDouble(separationComma[4]);
+				
+				line = bufferedReader.readLine();
 
 				Product product = new Product(consico, separationComma[1],value);
 				ManipulateData manipulateData = new ManipulateData(product, quantity, stock);
-				System.out.println(manipulateData);*/
+				System.out.println(manipulateData);
+
 			}
 		} catch (IOException e) {// Generic 
 			System.out.print("Error " + e.getMessage());
